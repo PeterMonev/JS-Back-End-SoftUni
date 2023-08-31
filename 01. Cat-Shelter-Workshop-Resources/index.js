d4dc30f89a6fdf15c6d1f1cbf4abf8a246a79dd2
PeterMonev <monevpeter@gmail.com>
2023-08-31 22:47:37 +0300

    Create index.js

---
+++ 01. Cat-Shelter-Workshop-Resources/index.js
@@ -0,0 +1,13 @@
+const http = require('http');
+const homePage = require('./home')
+
+const server = http.createServer((req,res)=>{
+    res.writeHead(200,{
+        'Content-Type': 'text/html'
+    });
+    
+    res.write(homePage);
+    res.end();
+});
+
+server.listen(5000,()=> console.log('Server is listening on port 5000'))

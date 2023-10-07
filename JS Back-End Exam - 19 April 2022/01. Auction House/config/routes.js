1342131ec995e86e443cdf706981032960aead0f
PeterMonev <monevpeter@gmail.com>
2023-10-07 12:45:43 +0300

    Create routes.js

---
+++ JS Back-End Exam - 19 April 2022/01. Auction House/config/routes.js
@@ -0,0 +1,14 @@
+const authController = require("../controllers/authController");
+const homeController = require("../controllers/homeController");
+const auctionController = require("../controllers/auctionController");
+
+module.exports = (app) => {
+  app.use("/", homeController);
+  app.use("/auth", authController);
+  app.use("/auction", auctionController);
+
+  app.get("/*", (req, res) => {
+    res.render("404")
+  })
+
+};

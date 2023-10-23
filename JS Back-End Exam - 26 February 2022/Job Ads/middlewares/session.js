136958a4cac6cc730ff3086e2876258cf907a86d
PeterMonev <monevpeter@gmail.com>
2023-10-23 21:50:57 +0300

    Create session.js

---
+++ JS Back-End Exam - 26 February 2022/Job Ads/middlewares/session.js
@@ -0,0 +1,17 @@
+const { verifyToken } = require("../services/userService");
+
+module.exports = (app) => (req, res, next) => {
+  const token = req.cookies.token;
+  if (token) {
+    try {
+      const userData = verifyToken(token);
+      req.user = userData;
+      res.locals.email = userData.email;
+    } catch (error) {
+      res.clearCookie("token");
+      res.redirect("/auth/login");
+      return;
+    }
+  }
+  next();
+};

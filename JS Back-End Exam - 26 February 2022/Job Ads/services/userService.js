2ddd7354788f295b6f7983f376aae08de6bd618e
PeterMonev <monevpeter@gmail.com>
2023-10-24 09:34:00 +0300

    Create userService.js

---
+++ JS Back-End Exam - 26 February 2022/Job Ads/services/userService.js
@@ -0,0 +1,82 @@
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const User = require("../models/User");
+
+const JWT_SECRET = "dasidos";
+
+async function register(email, password, skills) {
+  const existing = await User.findOne({ email }).collation({
+    locale: "en",
+    strength: 2,
+  });
+  if (existing) {
+    throw new Error("User already registered!");
+  }
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const user = await User.create({
+    email,
+    hashedPassword,
+    skills
+  });
+
+  return createSession(user);
+}
+
+async function login(email, password) {
+  const user = await User.findOne({ email }).collation({
+    locale: "en",
+    strength: 2,
+  });
+  if (!user) {
+    throw new Error("Incorrect username or password");
+  }
+
+  const hasMatch = await bcrypt.compare(password, user.hashedPassword);
+
+  if (!hasMatch) {
+    throw new Error("Incorrect username or password");
+  }
+
+  return createSession(user);
+}
+
+function createSession({ _id, email, skills }) {
+  const payload = {
+    _id,
+    email,
+    skills
+  };
+
+  return jwt.sign(payload, JWT_SECRET);
+}
+
+function verifyToken(token) {
+  return jwt.verify(token, JWT_SECRET);
+}
+
+async function getUserById(userId) {
+  return await User.findById(userId).lean();
+}
+
+async function updateUser(userId, list) {
+    let ExistingUser = await User.findById(userId);
+    ExistingUser.myAd = list;
+    
+    await ExistingUser.save();
+
+}
+
+async function findByEmail(email){
+   return await User.findOne({email: email});
+}
+
+module.exports = {
+  register,
+  login,
+  verifyToken,
+  getUserById,
+  updateUser,
+  findByEmail,
+};

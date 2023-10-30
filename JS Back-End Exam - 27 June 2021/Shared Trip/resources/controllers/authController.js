bb33f9f57367c5022f9d21efe3af6f37941fd147
PeterMonev <monevpeter@gmail.com>
2023-10-30 09:13:47 +0200

    Create authController.js

---
+++ JS Back-End Exam - 27 June 2021/Shared Trip/resources/controllers/authController.js
@@ -0,0 +1,74 @@
+const express = require("express");
+const { isGuest, hasUser } = require("../middlewares/guards");
+const { register,login } = require("../services/userService");
+const { parseError } = require("../util/parser");
+
+const authController = require("express").Router();
+
+authController.get("/register",isGuest(), (req, res) => {
+  res.render("register", {
+    title: "Register Page",
+  });
+});
+
+authController.post("/register",isGuest(), async (req, res) => {
+  console.log(req.body)
+  try {
+    if (req.body.email == "" || req.body.password == "") {
+      throw new Error("All fields are required");
+    }
+    if (req.body.password != req.body.repass) {
+      throw new Error("Passwords dont match");
+    }
+
+    if(req.body.password.length < 4){
+      throw new Error("Password must be at least 4 characters");
+    }
+
+    const token = await register(req.body.email, req.body.password, req.body.gender);
+
+    res.cookie("token", token);
+    res.redirect("/");
+  } catch (error) {
+    const errors = parseError(error);
+
+    res.render("register", {
+      title: "Register Page",
+      errors,
+      body: {
+        email: req.body.email,
+      },
+    });
+  }
+});
+
+authController.get("/login",isGuest(), (req, res) => {
+  res.render("login", {
+    title: "Login Page",
+  });
+});
+
+authController.post("/login",isGuest(), async (req, res) => {
+  try { 
+  const token = await login(req.body.email, req.body.password);
+
+  res.cookie('token', token);
+  res.redirect('/');
+  } catch (error) {
+    const errors = parseError(error);
+    res.render("login", {
+      title: "Login Page",
+      errors,
+      body: {
+        email: req.body.email,
+      },
+    });
+  }
+});
+
+authController.get('/logout',hasUser(), (req, res) => {
+  res.clearCookie('token');
+  res.redirect('/');
+})
+
+module.exports = authController;

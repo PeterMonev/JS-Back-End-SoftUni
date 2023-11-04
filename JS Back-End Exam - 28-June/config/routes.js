f877b1bbfa7932aa0440920696c185fd335af402
PeterMonev <monevpeter@gmail.com>
2023-11-04 10:57:54 +0200

    Create routes.js

---
+++ JS Back-End Exam - 28-June/config/routes.js
@@ -0,0 +1,14 @@
+const authController = require("../controllers/authController");
+const homeController = require("../controllers/homeController");
+const playController = require("../controllers/playController");
+
+
+module.exports = (app) => {
+  app.use("/", homeController);
+  app.use("/auth", authController);
+  app.use("/play", playController);
+
+  // app.get("/*", (req, res) => {
+  //   res.render("404");
+  // });
+};

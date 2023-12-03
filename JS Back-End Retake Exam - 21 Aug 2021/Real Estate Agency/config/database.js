9156a2be3d99006a3f946aa8abbd5f523a1f500d
PeterMonev <monevpeter@gmail.com>
2023-12-03 10:32:51 +0200

    Create database.js

---
+++ JS Back-End Retake Exam - 21 Aug 2021/Real Estate Agency/config/database.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+const CONNESTION_STRING =  process.env.DATABASE_CONNECTION_STRING ||
+  "mongodb://0.0.0.0:27017/real-estate";
+
+mongoose.set("strictQuery", true);
+module.exports = async (app) => {
+  try {
+      await mongoose.connect(CONNESTION_STRING, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+   
+    });
+    console.log('Database connected');
+    
+  } catch (err) {
+    console.log(err.message);
+    process.exit(1);
+  }
+};

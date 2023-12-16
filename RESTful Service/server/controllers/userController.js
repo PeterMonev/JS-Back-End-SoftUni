0c26423d32ec8f19504318c110abf64dd51976a8
PeterMonev <monevpeter@gmail.com>
2023-12-16 12:30:05 +0200

    Create userController.js

---
+++ RESTful Service/server/controllers/userController.js
@@ -0,0 +1,20 @@
+const router = require('express').Router();
+
+router.post('/regsiter', async(req, res) => {
+     const {username, email, password, repeatPassword } = req.body;
+
+     const token = await authService.register(username, email, password, repeatPassword);
+
+     res.cookie('auth', token);
+   
+});
+
+router.post('/login', (req, res) => {
+
+});
+
+router.get('/logout', (req, res) => {
+
+});
+
+module.exports = router;

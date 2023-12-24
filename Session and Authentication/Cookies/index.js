e5033eae8c0c1056523f644cbe5ad7eff921f7b4
PeterMonev <monevpeter@gmail.com>
2023-12-24 10:11:35 +0200

    Create index.js

---
+++ Session and Authentication/Cookies/index.js
@@ -0,0 +1,51 @@
+const server = require("http")
+  .createServer((req, res) => {
+    if (req.url == "/") {
+      action(req, res);
+    } else {
+      res.writeHead(404);
+      res.end();
+    }
+  })
+  .listen(3000);
+
+function parseCookie(req) {
+  if (req.headers.cookie) {
+    const cookies = Object.fromEntries(
+      req.headers.cookie
+        .split(";")
+        .map((c) => c.trim())
+        .map((c) => c.split("="))
+    );
+    console.log(cookies);
+
+    return cookies;
+  }
+
+  return {};
+}
+
+const sessions = {};
+
+function action(req, res) {
+  const cookies = parseCookie(req);
+  const sessionId = cookies.sessionId || (Math.random() * 999999).toString(16);
+  const session = sessions[sessionId] || {};
+
+  session.visited = (session.visited || 0) + 1;
+
+  sessions[sessionId] = session;
+
+  res.writeHead(200, {
+    "Set-Cookie": `sessionId=${sessionId}; httpOnly`,
+  });
+
+  //||
+//   res.writeHead(200, [
+//     "Set-Cookie", `sessionId=${sessionId}; httpOnly`,
+//     "Set-Cookie", `theme=dark`
+//   ]);
+
+  res.write(`<p>Hello</p><p>You have visited this page time: ${session.visited}</p>`);
+  res.end();
+}

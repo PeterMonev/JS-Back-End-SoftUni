486fcd01dad6d9240b6afce494704631a6e4a316
PeterMonev <monevpeter@gmail.com>
2023-12-27 10:10:17 +0200

    Create homeCotroller.js

---
+++ Workshop ExpressJS and Templating/03. Cubicle-Workshop-Part-1-Resources/controllers/homeCotroller.js
@@ -0,0 +1,23 @@
+const router = require('express').Router();
+const cubeService = require('../services/cubeService');
+
+
+router.get('/', (req, res) => {
+    let { search, from, to } = req.query;
+
+
+    const cubes = cubeService.getAll(search, from, to);
+
+    res.render('index', { cubes,search, from, to })
+});
+
+router.get('/about', (req, res) => {
+    res.render('about')
+});
+
+router.post('/123', (req, res) => {
+    console.log(req.body);
+    res.send('Successful form submit!')
+})
+
+module.exports = router; 

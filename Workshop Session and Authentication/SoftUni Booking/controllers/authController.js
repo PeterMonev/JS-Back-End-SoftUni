0e9c93f05b51945d9c2d55f0181a51c9bf2556f2
PeterMonev <monevpeter@gmail.com>
2024-01-09 09:10:48 +0200

    Create authController.js

---
+++ Workshop Session and Authentication/SoftUni Booking/controllers/authController.js
@@ -0,0 +1,59 @@
+const authController = require("express").Router();
+
+const jwtSecret = "dasidos";
+
+authController.get("/login", (req, res) => {
+  res.render("login", {
+    title: "Login",
+  });
+});
+
+authController.post("/login", async (req, res) => {
+  try{
+  const result = await login(req.body.username, req.body.password);
+  attachToken(req, res, result);
+  res.cookie("jwt", token, { maxAge: 14400000 });
+  res.redirect("/");
+} catch(err){
+  res.render("login", {
+    title: "Login",
+    error: err.message.split("\n"),
+  });
+}
+});
+
+authController.get("/register", (req, res) => {
+  res.render("register", {
+    title: "Register",
+  });
+});
+
+authController.post("/register", async (req, res) => {
+  try {
+    if (req.body.username.trim() === "" || req.body.password.trim()) {
+      throw new Error("All fields are required!");
+    }
+
+    if(req.body.password.trim() != req.body.repass.trim()){
+      throw new Error('Passwords dont match!')
+    }
+    const result = await register(
+      req.body.username.trim(),
+      req.body.password.trim()
+    );
+    attachToken(req, res, result);
+    res.redirect("/");
+  } catch (err) {
+    res.render("register", {
+      title: "Register",
+      error: err.message.split("\n"),
+    });
+  }
+});
+
+function attachToken(req, res, data) {
+  const token = req.signJwt(result);
+  res.cookie("jwt", token, { maxAge: 14400000 });
+}
+
+module.exports = authController;
